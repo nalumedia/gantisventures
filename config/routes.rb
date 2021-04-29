31a4b5eb907fabaa478d9f6b0accce89f11f2e3b
nalumedia <ryan.dranginis@gmail.com>
2021-04-28 20:32:03 -0700

    added moves

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :moves
   devise_for :users
   devise_for :views
   resources :transactions
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_28_141516) do
+ActiveRecord::Schema.define(version: 2021_04_29_001845) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "nameLstring"
     t.string "horizon"
     t.string "target_exit"
     t.text "notes"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "moves", force: :cascade do |t|
+    t.string "name"
+    t.integer "amount"
+    t.text "notes"
+    t.integer "account_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
